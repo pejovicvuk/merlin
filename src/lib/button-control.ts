89518d809b0820271fb98afb3595c03f0b49de6a
Dejan Jelovic <dejan@jelovic.com>
2025-01-28 18:18:26 +0100

    Added support for disabling the button control; 0.0.13.

--- src/lib/button-control.ts
+++ src/lib/button-control.ts
@@ -1,7 +1,21 @@
 import { HtmlControl } from "./html-control.js";
 
 const styleSheet = new CSSStyleSheet();
-styleSheet.replaceSync(':host { border: 1px solid ButtonBorder; background-color: ButtonFace; color: ButtonText; padding: 0.25lh 0.8ch; cursor: default; user-select: none; display: inline-block; }');
+styleSheet.replaceSync(`
+    :host {
+        border: 1px solid ButtonBorder;
+        background-color: ButtonFace;
+        color: ButtonText;
+        padding: 0.25lh 0.8ch;
+        cursor: default;
+        user-select: none;
+        display: inline-block;
+    }
+    :host(*[aria-disabled="true"]) {
+        color: GrayText;
+        border-color: GrayText;
+    }
+`);
 
 export class ButtonControl extends HtmlControl {
     constructor() {
@@ -11,5 +25,17 @@ export class ButtonControl extends HtmlControl {
         this.adoptStyleSheet(styleSheet);
 
         shadow.innerHTML = '<slot></slot>'
+    }
+
+    override onDisabledChanged() {
+        super.onDisabledChanged();
+
+        const disabled = this.disabled === true;
+        if (disabled) {
+            this.setAttribute('aria-disabled', 'true');
+        }
+        else {
+            this.removeAttribute('aria-disabled');
+        }
     }
 }

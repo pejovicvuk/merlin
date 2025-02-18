03068644eaccff45ce5c1f420903d8ec2dedb065
Dejan Jelovic <dejan@jelovic.com>
2025-02-18 06:47:44 +0100

    Changed the items control to use slots.

--- src/lib/items-control.ts
+++ src/lib/items-control.ts
@@ -5,15 +5,18 @@ import { HtmlControl, HtmlControlBindableProperty } from "./html-control.js";
 const standardTemplate = document.createElement('template');
 standardTemplate.innerHTML = '<text-block text="this"></text-block>';
 
+const shadowHtml = '<slot name="item-template"></slot><div part="container"></div>';
+
 export class ItemsControl extends HtmlControl implements HtmlControlBindableProperty<'items', Iterable<any>> {
     static override bindableProperties = [...HtmlControl.bindableProperties, 'items'];
 
     #displayedItems?: Iterable<any>;
+    #slotCount = 0;
 
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open", delegatesFocus: true });
-        shadow.innerHTML = '<slot name="item-template"></slot><div part="container"></div>';
+        shadow.innerHTML = shadowHtml;
 
         const slot = this.#itemTemplateSlot;
         slot.addEventListener('slotchange', ItemsControl.#onSlotChangeShared);
@@ -28,6 +31,9 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
         if (this.#displayedItems === undefined) return;
 
         const div = this.itemsContainer;
+        for (const slot of div.children) {
+            for (const el of (slot as HTMLSlotElement).assignedElements()) el.remove();
+        }
         div.innerHTML = '';
 
         const template = this.#itemTemplateContent;
@@ -36,8 +42,16 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
 
             ctl.append(template.cloneNode(true));
             ctl.model = item; // safe as we are descendant of BindableControl so if we are created then so is BindalbeControl
-            div.appendChild(ctl);
-        }
+
+            const slotName = 'i-' + this.#slotCount++;
+
+            ctl.slot = slotName;
+            this.appendChild(ctl);
+
+            const slot = document.createElement('slot');
+            slot.name = slotName;
+            div.appendChild(slot);
+    }
     }
 
     get #itemTemplateSlot() {
@@ -55,7 +69,7 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
     }
 
     get itemsContainer(): HTMLElement {
-        return this.shadowRoot!.querySelector('div')!
+        return this.shadowRoot!.querySelector('div[part="container"]')!;
     }
 
     onItemsChanged() {
@@ -77,7 +91,17 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
         }
 
         const div = this.itemsContainer;
+        for (const slot of div.children) {
+            for (const el of (slot as HTMLSlotElement).assignedElements()) el.remove();
+        }
         div.innerHTML = '';
+
+        let chNum = this.children.length;
+        while (chNum-- > 0) {
+            const ch = this.children[chNum];
+
+            if (ch instanceof HTMLSlotElement && ch.name.startsWith('i-')) ch.remove();
+        }
 
         this.#displayedItems = items;
 
@@ -94,7 +118,15 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
                 const ctl = this.createItemContainer();
                 ctl.append(template.cloneNode(true));
                 ctl.model = item; // safe as we are descendant of BindableControl so if we are created then so is BindalbeControl
-                div.appendChild(ctl);
+
+                const slotName = 'i-' + this.#slotCount++;
+
+                ctl.slot = slotName;
+                this.appendChild(ctl);
+
+                const slot = document.createElement('slot');
+                slot.name = slotName;
+                div.appendChild(slot);
             }
         }
     }
@@ -108,7 +140,7 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
 
         while(same-- > 0) {
             const item = arr[index];
-            const ctl = div.children[index++] as BindableControl;
+            const ctl = (div.children[index++] as HTMLSlotElement).assignedElements()[0] as BindableControl;
             ctl.model = item;
         }
 
@@ -117,16 +149,28 @@ export class ItemsControl extends HtmlControl implements HtmlControlBindableProp
             const ctl = this.createItemContainer();
             ctl.append(template.cloneNode(true));
             ctl.model = arr[index]; // safe as we are descendant of BindableControl so if we are created then so is BindalbeControl
+
+
+            const slotName = 'i-' + this.#slotCount++;
+
+            ctl.slot = slotName;
+            this.appendChild(ctl);
+
+            const slot = document.createElement('slot');
+            slot.name = slotName;
+
             if (index < div.childElementCount) {
-                div.insertBefore(ctl, div.children[index++]);
+                div.insertBefore(slot, div.children[index++]);
             }
             else {
-                div.appendChild(ctl);
+                div.appendChild(slot);
             }
         }
 
         while(deleted > 0) {
-            div.children[index + --deleted].remove();
+            const slot = div.children[index + --deleted] as HTMLSlotElement;
+            for (const assigned of slot.assignedElements()) assigned.remove();
+            slot.remove();
         }
     };
 
